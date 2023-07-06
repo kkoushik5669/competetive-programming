fda414b7ab81fa506894698e6388ed5cffc3b7c8
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2023-07-06 23:10:25 +0530

    Create numberofwayshorsesattackeachother.cpp

---
+++ template/maths/numberofwayshorsesattackeachother.cpp
@@ -0,0 +1,2 @@
+//this is the formulae for which two horses can be placed on a chess board so that they donot attack eachother a(n) = (n - 1)*(n + 4)*(n^2 - 3*n + 4)/2;
+//it can also be shown as k*k*(k*k-1)/2-4*(k-1)*(k-2)
