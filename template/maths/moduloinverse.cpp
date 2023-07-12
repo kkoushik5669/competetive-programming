753ae1301a1a3b3d12be1c6f79afda2a91c1e8e0
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2023-07-12 18:10:33 +0530

    Create moduloinverse.cpp

---
+++ template/maths/moduloinverse.cpp
@@ -0,0 +1,40 @@
+int gcd(int a, int b);
+ 
+// To compute x raised to power y under modulo M
+int power(int x, unsigned int y, unsigned int M);
+ 
+// Function to find modular inverse of a under modulo M
+// Assumption: M is prime
+void modInverse(int A, int M)
+{
+    int g = gcd(A, M);
+    if (g != 1)
+        cout << "Inverse doesn't exist";
+    else {
+        // If a and m are relatively prime, then modulo
+        // inverse is a^(m-2) mode m
+        cout << "Modular multiplicative inverse is "
+             << power(A, M - 2, M);
+    }
+}
+ 
+// To compute x^y under modulo m
+int power(int x, unsigned int y, unsigned int M)
+{
+    if (y == 0)
+        return 1;
+ 
+    int p = power(x, y / 2, M) % M;
+    p = (p * p) % M;
+ 
+    return (y % 2 == 0) ? p : (x * p) % M;
+}
+ 
+// Function to return gcd of a and b
+int gcd(int a, int b)
+{
+    if (a == 0)
+        return b;
+    return gcd(b % a, a);
+}
+ 
