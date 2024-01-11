d4f095b1aef92d6213f9a252a02aa6f090449e3f
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2024-01-11 13:44:12 +0530

    Update disjointset.cpp

--- template/graphs/disjointset.cpp
+++ template/graphs/disjointset.cpp
@@ -1,33 +1,15 @@
-class DisjointSet {
-    vector<int> rank, parent;
-public:
-    DisjointSet(int n) {
-        rank.resize(n + 1, 0);
-        parent.resize(n + 1);
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i;
-        }
-    }
-
-    int findUPar(int node) {
-        if (node == parent[node])
-            return node;
-        return parent[node] = findUPar(parent[node]);
-    }
-
-    void unionByRank(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
-        if (rank[ulp_u] < rank[ulp_v]) {
-            parent[ulp_u] = ulp_v;
-        }
-        else if (rank[ulp_v] < rank[ulp_u]) {
-            parent[ulp_v] = ulp_u;
-        }
-        else {
-            parent[ulp_v] = ulp_u;
-            rank[ulp_u]++;
-        }
+struct DSU {
+    vector<int> e;
+    void init(int n) { e = vector<int>(n, -1); }
+    int get(int x) { return (e[x] < 0 ? x : e[x] = get(e[x])); }
+    bool sameSet(int x, int y) { return get(x) == get(y); }
+    int size(int x) { return -e[get(x)]; }
+    bool unite(int x, int y) {
+        x = get(x), y = get(y);
+        if (x == y) return 0;
+        if (e[x] > e[y]) swap(x, y);
+        e[x] += e[y];
+        e[y] = x;
+        return 1;
     }
 };
