69293661ebc360a3c1f5574d25f6edf97acc891c
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2024-01-13 16:54:50 +0530

    Update phiofanumber.cpp

--- maths/phiofanumber.cpp
+++ maths/phiofanumber.cpp
@@ -11,3 +11,9 @@ int phi(int n) {
         result -= result / n;
     return result;
 }
+//some properties of phi;
+//phi(p) =p-1 when p is a prime
+//phi(p ^k)=p^k-p^k-1
+//phi(a*b)=phi(a)*phi(b) where a and b are relatively prime
+//phi(ab)=phi(a)*phi(b)*(d/phi(d)) with d=__gcd(a,b)
+//phi(n)=n(1-1/p1)(1-1/p2)(1-1/p3)..with this formula calculate phi(n)
