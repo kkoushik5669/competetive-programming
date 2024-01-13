1dec3a3fe607f4e200338a90e4ad9d48d05d1678
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2024-01-13 15:39:09 +0530

    Create extendedeuclidean.cpp

---
+++ maths/extendedeuclidean.cpp
@@ -0,0 +1,2 @@
+template<typename T>T extended_euclid(T a, T b, T &x, T &y){T xx=0,yy=1;y=0;x=1;while(b){T q=a/b,t=b;b=a%b;a=t;t=xx;xx=x-q*xx;x=t;t=yy;yy=y-q*yy;y=t;}return a;}
+//ax+by=g and bx+(a mod b)y=g as g=__gcd(a,b) =__gcd(b,a mod b)
