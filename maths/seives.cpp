50fbb0138ffce2bd8be361134aa2b4972f4a72c0
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2024-01-13 15:30:44 +0530

    Update seives.cpp

--- maths/seives.cpp
+++ maths/seives.cpp
@@ -2,3 +2,4 @@ vector<int>primes;vector<bool>isprime(N);
 bool prime(ll n) {if (n < 2) return false;for (int x = 2; x*x <= n; x++) {if (n%x == 0) return false;}return true;}
 void seives()
 {int i;for(i=2;i*i<N;i++){if(isprime[i]==false){primes.push_back(i);for(int j=i*i;j<N;j+=i){isprime[j]=true;}}}for(;i<N;i++){if(isprime[i]==false){primes.push_back(i);}}}
+//O(nlnln(rootn))
