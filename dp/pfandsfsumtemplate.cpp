f4749429ce2ba439ea5662480c0c7404a19739f9
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2024-02-04 22:41:12 +0530

    Create pfandsfsumtemplate.cpp

---
+++ dp/pfandsfsumtemplate.cpp
@@ -0,0 +1,16 @@
+vector<ll>findps(vector<ll>a)
+{
+    int n=a.size();
+    vector<ll>pf(n);
+    pf[0]=a[0];
+    for(int i=1;i<n;i++)pf[i]=pf[i-1]+a[i];
+  return pf;
+}
+vector<ll>sf(vector<ll>a)
+{
+    int n=a.size();
+    vector<ll>sf(n);
+    sf[n-1]=a[n-1];
+    for(int i=n-2;i>=0;i--){sf[i]=sf[i+1]+a[i];}
+  return sf;
+}
