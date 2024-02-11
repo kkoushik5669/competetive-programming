b3476ce1dbb0cffb7e8d13ec2bf31d8085da2208
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2024-02-11 21:54:37 +0530

    Update wavelettree.cpp

--- datastructures/wavelettree.cpp
+++ datastructures/wavelettree.cpp
@@ -1,131 +1,113 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define gc getchar_unlocked
-#define fo(i,n) for(i=0;i<n;i++)
-#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
-#define ll long long
-#define si(x)	scanf("%d",&x)
-#define sl(x)	scanf("%lld",&x)
-#define ss(s)	scanf("%s",s)
-#define pi(x)	printf("%d\n",x)
-#define pl(x)	printf("%lld\n",x)
-#define ps(s)	printf("%s\n",s)
-#define deb(x) cout << #x << "=" << x << endl
-#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
-#define pb push_back
-#define mp make_pair
-#define F first
-#define S second
-#define all(x) x.begin(), x.end()
-#define clr(x) memset(x, 0, sizeof(x))
-#define sortall(x) sort(all(x))
-#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
-#define PI 3.1415926535897932384626
-typedef pair<int, int>	pii;
-typedef pair<ll, ll>	pl;
-typedef vector<int>		vi;
-typedef vector<ll>		vl;
-typedef vector<pii>		vpii;
-typedef vector<pl>		vpl;
-typedef vector<vi>		vvi;
-typedef vector<vl>		vvl;
-int mpow(int base, int exp); 
-void ipgraph(int m);
-void dfs(int u, int par);
-const int mod = 1000000007;
-const int N = 3e5, M = N;
-//=======================
-const int MAX = 1e6;
-vi g[N];
-int a[N];
-struct wavelet_tree{
-	#define vi vector<int>
-	#define pb push_back
-	int lo, hi;
-	wavelet_tree *l, *r;
-	vi b;
-	
-	//nos are in range [x,y]
-	//array indices are [from, to)
-	wavelet_tree(int *from, int *to, int x, int y){
-		lo = x, hi = y;
-		if(lo == hi or from >= to) return;
-		int mid = (lo+hi)/2;
-		auto f = [mid](int x){
-			return x <= mid;
-		};
-		b.reserve(to-from+1);
-		b.pb(0);
-		for(auto it = from; it != to; it++)
-			b.pb(b.back() + f(*it));
-		//see how lambda function is used here	
-		auto pivot = stable_partition(from, to, f);
-		l = new wavelet_tree(from, pivot, lo, mid);
-		r = new wavelet_tree(pivot, to, mid+1, hi);
-	}
-	
-	//kth smallest element in [l, r]
-	int kth(int l, int r, int k){
-		if(l > r) return 0;
-		if(lo == hi) return lo;
-		int inLeft = b[r] - b[l-1];
-		int lb = b[l-1]; //amt of nos in first (l-1) nos that go in left 
-		int rb = b[r]; //amt of nos in first (r) nos that go in left
-		if(k <= inLeft) return this->l->kth(lb+1, rb , k);
-		return this->r->kth(l-lb, r-rb, k-inLeft);
-	}
-	
-	//count of nos in [l, r] Less than or equal to k
-	int LTE(int l, int r, int k) {
-		if(l > r or k < lo) return 0;
-		if(hi <= k) return r - l + 1;
-		int lb = b[l-1], rb = b[r];
-		return this->l->LTE(lb+1, rb, k) + this->r->LTE(l-lb, r-rb, k);
-	}
-  
-	//count of nos in [l, r] equal to k
-	int count(int l, int r, int k) {
-		if(l > r or k < lo or k > hi) return 0;
-		if(lo == hi) return r - l + 1;
-		int lb = b[l-1], rb = b[r], mid = (lo+hi)/2;
-		if(k <= mid) return this->l->count(lb+1, rb, k);
-		return this->r->count(l-lb, r-rb, k);
-	}
-	~wavelet_tree(){
-		delete l;
-		delete r;
-	}
-};
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	srand(time(NULL));
-	int i,n,k,j,q,l,r;
-	cin >> n;
-	fo(i, n) cin >> a[i+1];
-	wavelet_tree T(a+1, a+n+1, 1, MAX);
-	cin >> q;
-	while(q--){
-		int x;
-		cin >> x;
-		cin >> l >> r >> k;
-		if(x == 0){
-			//kth smallest
-			cout << "Kth smallest: ";
-			cout << T.kth(l, r, k) << endl;
-		}
-		if(x == 1){
-			//less than or equal to K
-			cout << "LTE: ";
-			cout << T.LTE(l, r, k) << endl;
-		}
-		if(x == 2){
-			//count occurence of K in [l, r]
-			cout << "Occurence of K: ";
-			cout << T.count(l, r, k) << endl;
-		}
-	}
-	return 0;
-} 
 
+const int MAXN = (int)3e5 + 9;
+const int MAXV = (int)1e9 + 9; //maximum value of any element in array
+
+//array values can be negative too, use appropriate minimum and maximum value
+struct wavelet_tree {
+  int lo, hi;
+  wavelet_tree *l, *r;
+  int *b, *c, bsz, csz; // c holds the prefix sum of elements
+
+  wavelet_tree() {
+    lo = 1;
+    hi = 0;
+    bsz = 0;
+    csz = 0, l = NULL;
+    r = NULL;
+  }
+
+  void init(int *from, int *to, int x, int y) {
+    lo = x, hi = y;
+    if(from >= to) return;
+    int mid = (lo + hi) >> 1;
+    auto f = [mid](int x) {
+      return x <= mid;
+    };
+    b = (int*)malloc((to - from + 2) * sizeof(int));
+    bsz = 0;
+    b[bsz++] = 0;
+    c = (int*)malloc((to - from + 2) * sizeof(int));
+    csz = 0;
+    c[csz++] = 0;
+    for(auto it = from; it != to; it++) {
+      b[bsz] = (b[bsz - 1] + f(*it));
+      c[csz] = (c[csz - 1] + (*it));
+      bsz++;
+      csz++;
+    }
+    if(hi == lo) return;
+    auto pivot = stable_partition(from, to, f);
+    l = new wavelet_tree();
+    l->init(from, pivot, lo, mid);
+    r = new wavelet_tree();
+    r->init(pivot, to, mid + 1, hi);
+  }
+  //kth smallest element in [l, r]
+  //for array [1,2,1,3,5] 2nd smallest is 1 and 3rd smallest is 2
+  int kth(int l, int r, int k) {
+    if(l > r) return 0;
+    if(lo == hi) return lo;
+    int inLeft = b[r] - b[l - 1], lb = b[l - 1], rb = b[r];
+    if(k <= inLeft) return this->l->kth(lb + 1, rb, k);
+    return this->r->kth(l - lb, r - rb, k - inLeft);
+  }
+  //count of numbers in [l, r] Less than or equal to k
+  int LTE(int l, int r, int k) {
+    if(l > r || k < lo) return 0;
+    if(hi <= k) return r - l + 1;
+    int lb = b[l - 1], rb = b[r];
+    return this->l->LTE(lb + 1, rb, k) + this->r->LTE(l - lb, r - rb, k);
+  }
+  //count of numbers in [l, r] equal to k
+  int count(int l, int r, int k) {
+    if(l > r || k < lo || k > hi) return 0;
+    if(lo == hi) return r - l + 1;
+    int lb = b[l - 1], rb = b[r];
+    int mid = (lo + hi) >> 1;
+    if(k <= mid) return this->l->count(lb + 1, rb, k);
+    return this->r->count(l - lb, r - rb, k);
+  }
+  //sum of numbers in [l ,r] less than or equal to k
+  int sum(int l, int r, int k) {
+    if(l > r or k < lo) return 0;
+    if(hi <= k) return c[r] - c[l - 1];
+    int lb = b[l - 1], rb = b[r];
+    return this->l->sum(lb + 1, rb, k) + this->r->sum(l - lb, r - rb, k);
+  }
+  ~wavelet_tree() {
+    delete l;
+    delete r;
+  }
+};
+wavelet_tree t;
+int a[MAXN];
+int main() {
+  int i, j, k, n, m, q, l, r;
+  cin >> n;
+  for(i = 1; i <= n; i++) cin >> a[i];
+  t.init(a + 1, a + n + 1, -MAXV, MAXV);
+  //beware! after the init() operation array a[] will not be same
+  cin >> q;
+  while(q--) {
+    int x;
+    cin >> x;
+    cin >> l >> r >> k;
+    if(x == 0) {
+      //kth smallest
+      cout << t.kth(l, r, k) << endl;
+    } else if(x == 1) {
+      //less than or equal to K
+      cout << t.LTE(l, r, k) << endl;
+    } else if(x == 2) {
+      //count occurence of K in [l, r]
+      cout << t.count(l, r, k) << endl;
+    }
+    if(x == 3) {
+      //sum of elements less than or equal to K in [l, r]
+      cout << t.sum(l, r, k) << endl;
+    }
+  }
+  return 0;
+}
