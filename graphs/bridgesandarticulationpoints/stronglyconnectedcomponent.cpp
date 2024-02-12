4f4cf84eae1485d17fd2622f290d7bbd126f6400
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2024-02-12 22:40:07 +0530

    Update stronglyconnectedcomponent.cpp

--- graphs/bridgesandarticulationpoints/stronglyconnectedcomponent.cpp
+++ graphs/bridgesandarticulationpoints/stronglyconnectedcomponent.cpp
@@ -1,57 +1,68 @@
-vector<vector<int>> adj, adj_rev;
-vector<bool> used;
-vector<int> order, component;
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 3e5 + 9;
 //What is a condensed graph?? It is formed by making a graph from different strongly connected components
 //What is the root of a condensed graph .It is the one scc which has the maximum tout
 //for each scc the one with the maximum tout is where we need to apply dfs from
 //how to optimally find the nodes for maximum tout just find the topo sort brooo
 //It can be proved that applying the dfs on a straight graph and a reverse graph would give you the same scc
 //It is optimal to apply dfs on reverse graph because it will not visit other useless scc's .
-void dfs1(int v) {
-    used[v] = true;
-
-    for (auto u : adj[v])
-        if (!used[u])
-            dfs1(u);
-
-    order.push_back(v);
+// given a directed graph return the minimum number of edges to be added so that the whole graph  become an SCC
+bool vis[N];
+vector<int> g[N], r[N], G[N], vec; //G is the condensed graph
+void dfs1(int u) {
+  vis[u] = 1;
+  for(auto v: g[u]) if(!vis[v]) dfs1(v);
+  vec.push_back(u);
 }
 
-void dfs2(int v) {
-    used[v] = true;
-    component.push_back(v);
-
-    for (auto u : adj_rev[v])
-        if (!used[u])
-            dfs2(u);
+vector<int> comp;
+void dfs2(int u) {
+  comp.push_back(u);
+  vis[u] = 1;
+  for(auto v: r[u]) if(!vis[v]) dfs2(v);
 }
 
+int idx[N], in[N], out[N];
 int main() {
-    int n;
-    // ... read n ...
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
 
-    for (;;) {
-        int a, b;
-        // ... read next directed edge (a,b) ...
-        adj[a].push_back(b);
-        adj_rev[b].push_back(a);
+  int n, m;
+  cin >> n >> m;
+  for(int i = 1; i <= m; i++) {
+    int u, v;
+    cin >> u >> v;
+    g[u].push_back(v);
+    r[v].push_back(u);
+  }
+  for(int i = 1; i <= n; i++) if(!vis[i]) dfs1(i);
+  reverse(vec.begin(), vec.end());
+  memset(vis, 0, sizeof vis);
+  int scc = 0;
+  for(auto u: vec) {
+    if(!vis[u]) {
+      comp.clear();
+      dfs2(u);
+      scc++;
+      for(auto x: comp) idx[x]=scc;
     }
-
-    used.assign(n, false);
-
-    for (int i = 0; i < n; i++)
-        if (!used[i])
-            dfs1(i);
-
-    used.assign(n, false);
-    reverse(order.begin(), order.end());
-
-    for (auto v : order)
-        if (!used[v]) {
-            dfs2 (v);
-
-            // ... processing next component ...
-
-            component.clear();
-        }
+  }
+  for(int u = 1; u <= n; u++) {
+    for(auto v: g[u]) {
+      if(idx[u] != idx[v]) {
+        in[idx[v]]++, out[idx[u]]++;
+        G[idx[u]].push_back(idx[v]);
+      }
+    }
+  }
+  int needed_in=0, needed_out=0;
+  for(int i = 1; i <= scc; i++) {
+    if(!in[i]) needed_in++;
+    if(!out[i]) needed_out++;
+  }
+  int ans = max(needed_in, needed_out);
+  if(scc == 1) ans = 0;
+  cout << ans << '\n';
+  return 0;
 }
