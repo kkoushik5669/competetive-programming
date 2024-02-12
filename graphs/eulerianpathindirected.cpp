90489fc1f8f93f6da2a4a7a8e4711b81b5d67296
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2024-02-12 10:22:58 +0530

    Create eulerianpathindirected.cpp

---
+++ graphs/eulerianpathindirected.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 4e5 + 9;
+/*
+all the edges should be in the same connected component
+#directed graph: euler path: for all -> indeg = outdeg or nodes having indeg > outdeg = outdeg > indeg = 1 and for others in = out
+#directed graph: euler circuit: for all -> indeg = outdeg
+*/
+//euler path in a directed graph
+//it also finds circuit if it exists
+vector<int> g[N], ans;
+int done[N];
+void dfs(int u) {
+  while (done[u] < g[u].size()) dfs(g[u][done[u]++]);
+  ans.push_back(u);
+}
+int solve(int n) {
+  int edges = 0;
+  vector<int> in(n + 1, 0), out(n + 1, 0);
+  for (int u = 1; u <= n; u++) {
+    for (auto v : g[u]) in[v]++, out[u]++, edges++;
+  }
+  int ok = 1, cnt1 = 0, cnt2 = 0, root = 0;
+  for (int i = 1; i <= n; i++) {
+    if (in[i] - out[i] == 1) cnt1++;
+    if (out[i] - in[i] == 1) cnt2++, root = i;
+    if (abs(in[i] - out[i]) > 1) ok = 0;
+  }
+  if (cnt1 > 1 || cnt2 > 1) ok = 0;
+  if (!ok) return 0;
+  if (root == 0) {
+    for (int i = 1; i <= n; i++) if (out[i]) root = i;
+  }
+  if (root == 0){return 1; }//empty graph
+  dfs(root);
+  if (ans.size() != edges + 1) return 0; //connectivity
+  reverse(ans.begin(), ans.end());
+  return 1;
+}
+map<string, int> mp;
+string id[N];
+int T = 0;
+int32_t main() {
+  int n,m;
+  cin >> n>>m;
+  for (int i = 1; i <=m; i++) {
+    int a,b;cin>>a>>b;
+    g[a].push_back(b);
+  }
+  //eulerian circuit in directed graph.
+  int ok = solve(n);
+  if (!ok) return cout << "NO\n", 0;
+  cout << "YES\n";
+  for(auto it:ans){cout<<it<<" ";}cout<<endl;
+  return 0;
+}
+// https://codeforces.com/contest/508/problem/D
