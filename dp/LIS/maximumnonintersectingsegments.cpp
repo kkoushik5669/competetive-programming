99385a6525803f246c1c39f92e9560cd67bdb9d4
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2024-02-25 13:40:28 +0530

    Create maximumnonintersectingsegments.cpp

---
+++ dp/LIS/maximumnonintersectingsegments.cpp
@@ -0,0 +1,4 @@
+//How to do this??
+//Take an array A which stores the left end point at the position of right end point
+//as A[i]=x if i is the right end point and x is the left end point.
+//Now maximum size of non intersecting segments is just the LIS of the array.
