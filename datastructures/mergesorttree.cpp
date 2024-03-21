16769bf497b77800d7229b7dd6933dd03dfc7b79
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2024-03-22 00:01:34 +0530

    Create mergesorttree.cpp

---
+++ datastructures/mergesorttree.cpp
@@ -0,0 +1,3 @@
+//use the point update segment tree template just replace the int value with the vector val
+//and in merging function write the way you want to merge the two vectors
+// and also take care of the remaining functions according to the given query.
