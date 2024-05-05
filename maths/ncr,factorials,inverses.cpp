1bf7678f9cdf20db0fa96619d5e3b3fc0c66440f
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2024-05-05 10:13:00 +0530

    Update ncr,factorials,inverses.cpp

--- maths/ncr,factorials,inverses.cpp
+++ maths/ncr,factorials,inverses.cpp
@@ -1,5 +1,50 @@
-vector<ll>ifact,inverse,fact;
-void inv(ll p=mod){inverse.resize(maxn);inverse[0] = inverse[1] = 1;for (int i = 2; i <= N; i++){inverse[i] = inverse[p % i] * (p - p / i) % p;}}
-void InverseofFactorial(ll p=mod){ifact.resize(maxn);ifact[0] = ifact[1] = 1;for (int i = 2; i <= N; i++){ifact[i] = (inverse[i] * ifact[i - 1]) % p;}}
-void factorial(ll p=mod){fact.resize(maxn);fact[0] = 1;for (int i = 1; i <= N; i++) {fact[i] = (fact[i - 1] * i) % p;}}
-ll ncr(ll n,ll r,ll c = mod){ return mul(mul(ifact[r],ifact[n-r],c),fact[n],c); }  
+template<int mod>
+inline unsigned int down(unsigned int x) {
+    return x >= mod ? x - mod : x;
+}
+template<int mod>
+struct Modint {
+    unsigned int x;
+    Modint() = default;
+    Modint(unsigned int x) : x(x) {}
+    friend istream& operator>>(istream& in, Modint& a) {return in >> a.x;}
+    friend ostream& operator<<(ostream& out, Modint a) {return out << a.x;}
+    friend Modint operator+(Modint a, Modint b) {return down<mod>(a.x + b.x);}
+    friend Modint operator-(Modint a, Modint b) {return down<mod>(a.x - b.x + mod);}
+    friend Modint operator*(Modint a, Modint b) {return 1ULL * a.x * b.x % mod;}
+    friend Modint operator/(Modint a, Modint b) {return a * ~b;}
+    friend Modint operator^(Modint a, int b) {Modint ans = 1; for(; b; b >>= 1, a *= a) if(b & 1) ans *= a; return ans;}
+    //finding the inverse of a given number
+    friend Modint operator~(Modint a) {return a ^ (mod - 2);}
+    friend Modint operator-(Modint a) {return down<mod>(mod - a.x);}
+    friend Modint& operator+=(Modint& a, Modint b) {return a = a + b;}
+    friend Modint& operator-=(Modint& a, Modint b) {return a = a - b;}
+    friend Modint& operator*=(Modint& a, Modint b) {return a = a * b;}
+    friend Modint& operator/=(Modint& a, Modint b) {return a = a / b;}
+    friend Modint& operator^=(Modint& a, int b) {return a = a ^ b;}
+    friend Modint& operator++(Modint& a) {return a += 1;}
+    friend Modint operator++(Modint& a, int) {Modint x = a; a += 1; return x;}
+    friend Modint& operator--(Modint& a) {return a -= 1;}
+    friend Modint operator--(Modint& a, int) {Modint x = a; a -= 1; return x;}
+    friend bool operator==(Modint a, Modint b) {return a.x == b.x;}
+    friend bool operator!=(Modint a, Modint b) {return !(a == b);}
+};
+const int N=200005;
+const int mod = 1000000007;
+typedef Modint<mod> mint;
+struct combi{
+  int n; vector<mint> facts, finvs, invs;
+  combi(int _n): n(_n), facts(_n), finvs(_n), invs(_n){
+    facts[0] = finvs[0] = 1;
+    invs[1] = 1;
+    for (int i = 2; i < n; i++) invs[i] =  invs[mod % i] * (mod-mod / i);
+    for(int i = 1; i < n; i++){
+      facts[i] = facts[i - 1] * i;
+      finvs[i] = finvs[i - 1] * invs[i];
+    }
+  }
+  inline mint fact(int n) { return ((n<0) ? 0:facts[n]); }
+  inline mint finv(int n) { return ((n<0) ? 0:finvs[n]); }
+  inline mint inv(int n) { return ((n<0) ? 0:invs[n]); }
+  inline mint ncr(int n, int k) { return n < k or k < 0 ? 0 : facts[n] * finvs[k] * finvs[n-k]; }
+};combi C(N);
