61763ae05676f08a1c179e4bbf9243f02024926c
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2024-07-05 09:57:20 +0530

    Create Heavylightnodes.cpp

---
+++ datastructures/sqrtdecompositiontypes/Heavylightnodes.cpp
@@ -0,0 +1,3 @@
+//find the number of triangles in the graph with n nodes and m edges n<=100000 m <=100000
+//hint:- assume that the degree of a heavy nodes is greater than root n then the number of heavy nodes are less than root n
+//Assume a light node as a node with degree less than root n then the number of such nodes are greater than root n
